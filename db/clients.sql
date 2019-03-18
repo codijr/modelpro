810511d2d6948af90079f7b54f1506ca432a7671
rafero1 <rfl.avlar@outlook.com>
2019-03-18 11:28:52 -0300

    Atualiza tabelas MySQL

---
+++ db/clients.sql
@@ -0,0 +1,10 @@
+insert into clients (client_id, name, email, phone_number) values (1, 'Den Summerley', 'dsummerley0@edublogs.org', '1264737460');
+insert into clients (client_id, name, email, phone_number) values (2, 'Carmelle Keat', 'ckeat1@amazon.de', '2954953183');
+insert into clients (client_id, name, email, phone_number) values (3, 'Elfrida Mixer', 'emixer2@illinois.edu', '6001700974');
+insert into clients (client_id, name, email, phone_number) values (4, 'Leticia Dablin', 'ldablin3@admin.ch', '4667900706');
+insert into clients (client_id, name, email, phone_number) values (5, 'Bonnibelle Blasio', 'bblasio4@paypal.com', '7197505600');
+insert into clients (client_id, name, email, phone_number) values (6, 'Renato Escala', 'rescala5@epa.gov', '8904670139');
+insert into clients (client_id, name, email, phone_number) values (7, 'Indira Jacobson', 'ijacobson6@yolasite.com', '3697994276');
+insert into clients (client_id, name, email, phone_number) values (8, 'Hodge Comettoi', 'hcomettoi7@odnoklassniki.ru', '8984365012');
+insert into clients (client_id, name, email, phone_number) values (9, 'Donella Scholfield', 'dscholfield8@nasa.gov', '4282137785');
+insert into clients (client_id, name, email, phone_number) values (10, 'Kirby Blasio', 'kblasio9@creativecommons.org', '3062266342');
